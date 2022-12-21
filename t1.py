6de33dd051aa7d28a237797c0251db87cc6d8266
Otávio Augusto Dellay Secco <otavio.secco@estudante.uffs.edu.br>
2022-12-21 09:22:58 -0300

    Update t1.py

--- t1.py
+++ t1.py
@@ -3,7 +3,7 @@ from matplotlib import pyplot as plt
 import argparse
 import cv2
 
-image = cv2.imread('testimages/foto1_cap2.jpg')
+image = cv2.imread('testimages/foto1_cap1.jpg')
 imageOriginal = cv2.imread('testimages/foto1_gt.jpg')
 click_list = []
 
@@ -31,20 +31,19 @@ def orderPoints(pts):
     diff = np.diff(pts, axis = 1)
     rect[1] = pts[np.argmin(diff)]
     rect[3] = pts[np.argmax(diff)]
-    # return the ordered coordinates
     return rect
 
-def four_point_transform(image, pts):
+def four_point_topRightansform(image, pts):
 
     rect = orderPoints(pts)
-    (tl, tr, br, bl) = rect
+    (topLeft, topRight, bottomRight, bottomLeft) = rect
 
-    widthA = np.sqrt(((br[0] - bl[0]) ** 2) + ((br[1] - bl[1]) ** 2))
-    widthB = np.sqrt(((tr[0] - tl[0]) ** 2) + ((tr[1] - tl[1]) ** 2))
+    widthA = np.sqrt(((bottomRight[0] - bottomLeft[0]) ** 2) + ((bottomRight[1] - bottomLeft[1]) ** 2))
+    widthB = np.sqrt(((topRight[0] - topLeft[0]) ** 2) + ((topRight[1] - topLeft[1]) ** 2))
     maxWidth = max(int(widthA), int(widthB))
 
-    heightA = np.sqrt(((tr[0] - br[0]) ** 2) + ((tr[1] - br[1]) ** 2))
-    heightB = np.sqrt(((tl[0] - bl[0]) ** 2) + ((tl[1] - bl[1]) ** 2))
+    heightA = np.sqrt(((topRight[0] - bottomRight[0]) ** 2) + ((topRight[1] - bottomRight[1]) ** 2))
+    heightB = np.sqrt(((topLeft[0] - bottomLeft[0]) ** 2) + ((topLeft[1] - bottomLeft[1]) ** 2))
     maxHeight = max(int(heightA), int(heightB))
 
     dst = np.array([
@@ -54,7 +53,6 @@ def four_point_transform(image, pts):
         [0, maxHeight - 1]], dtype = "float32")
     m = cv2.getPerspectiveTransform(rect, dst)
     warped = cv2.warpPerspective(image, m, (maxWidth, maxHeight))
-    # return the warped image
     return warped
 
 def main():
@@ -64,7 +62,7 @@ def main():
     rect[1] = click_list[1]
     rect[2] = click_list[2]
     rect[3] = click_list[3]
-    warped = four_point_transform(image, rect)
+    warped = four_point_topRightansform(image, rect)
     # show the original and warped images
     cv2.namedWindow("Original", cv2.WINDOW_NORMAL)  
     cv2.resizeWindow("Original", imageOriginal.shape[0], imageOriginal.shape[1])         
